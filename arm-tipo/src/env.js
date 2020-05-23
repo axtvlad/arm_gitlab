d790ccdbd0d3f14ef597a79f3bc8985bed48f54f
axtvladislav <new-life-2020@mail.ru>
2020-05-23 20:29:13 +0600

    for ps.kz

--- arm-tipo/src/env.js
+++ arm-tipo/src/env.js
@@ -1,2 +1,2 @@
-export const BASE_URL1='https://arm-tipo.jcloud.kz/rest/api';
-export const BASE_URL='http://localhost:8080/rest/api';
+export const BASE_URL='https://arm-tipo.jcloud.kz/rest/api';
+// export const BASE_URL='http://localhost:8080/rest/api';
