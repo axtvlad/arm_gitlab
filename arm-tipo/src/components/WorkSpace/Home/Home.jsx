b62bf2cb6c7d940df62460a6c957864b574ba429
valentin.kulik <valentin.kulik.00@mail.ru>
2020-05-25 21:17:55 +0600

    [#15] Home page localization

--- arm-tipo/src/components/WorkSpace/Home/Home.jsx
+++ arm-tipo/src/components/WorkSpace/Home/Home.jsx
@@ -2,22 +2,29 @@ import React from 'react'
 import {Col, Row, Spin, Statistic} from 'antd';
 import TeamOutlined from "@ant-design/icons/lib/icons/TeamOutlined";
 import CopyOutlined from "@ant-design/icons/lib/icons/CopyOutlined";
+import {useTranslation} from "react-i18next";
 
 const Home = (props) => {
+
+    const {t} = useTranslation();
+
     return (
         <div className={'content'}>
             <Row gutter={16}>
                 <Col span={12}>
                     <Spin spinning={props.isFetching}>
                         <Statistic
-                            title="Всего пользователей в системе"
+                            title={t('totalUsersCount')}
                             value={props.usersCount}
                             prefix={<TeamOutlined/>}
                         />
                     </Spin>
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Всего документов" value={45} prefix={<CopyOutlined/>}/>
+                    <Statistic
+                        title={t('totalDocsCount')}
+                        value={45}
+                        prefix={<CopyOutlined/>}/>
                 </Col>
             </Row>
         </div>
