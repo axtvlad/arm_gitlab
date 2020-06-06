799712a24fa0764e016f3ed466627aaf8a2d1da7
axtvladislav <new-life-2020@mail.ru>
2020-06-06 11:27:30 +0600

    [#40] serv

--- arm-tipo/src/components/WorkSpace/Home/Home.jsx
+++ arm-tipo/src/components/WorkSpace/Home/Home.jsx
@@ -1,24 +1,63 @@
 import React from 'react'
-import {Col, Row, Spin, Statistic} from 'antd';
+import {Button, Col, Descriptions, Divider, PageHeader, Row, Spin, Statistic, Tag, Typography} from 'antd';
 import TeamOutlined from "@ant-design/icons/lib/icons/TeamOutlined";
 import CopyOutlined from "@ant-design/icons/lib/icons/CopyOutlined";
 import {useTranslation} from "react-i18next";
+import word_svg from "../../../svg/word.svg";
+import DownloadOutlined from "@ant-design/icons/lib/icons/DownloadOutlined";
 
 const Home = (props) => {
+    const {Title} = Typography;
 
     const {t} = useTranslation();
 
+    const getStatusColor = (id) => {
+        switch (id) {
+            case 1:
+                return 'red';
+            case 2:
+                return 'green';
+            case 3:
+                return 'blue';
+            default:
+                return 'orange';
+        }
+    };
 
-    let string = '';
+    const getStatusText = (id) => {
+        switch (id) {
+            case 1:
+                return 'Потерял актуальность';
+            case 2:
+                return 'Актуальный';
+            case 3:
+                return 'Новый';
+            default:
+                return 'Неизвестно'
+        }
+    };
 
-    const arr = ['a', 'b', 'c', 'd'];
+    const getDepartmentText = (id) => {
+        switch (id) {
+            case 1:
+                return 'МОН РК';
+            case 2:
+                return 'МТСЗ РК';
+            case 3:
+                return 'МИНЗДРАВ';
+            default:
+                return 'Неизвестно'
+        }
+    };
 
-    for (let i = 0; i < arr.length; i++) {
-        string += arr[i];
-    }
-
-    console.error(string);
-
+    const Content = ({children, extraContent}) => {
+        return (
+            <Row>
+                <div style={{flex: 1}}>{children}</div>
+                <div className="image">{extraContent}</div>
+            </Row>
+        );
+    };
 
     return (
         <div className={'content'}>
@@ -36,8 +75,66 @@ const Home = (props) => {
                     <Statistic
                         title={t('totalDocsCount')}
                         value={props.mainDocsCount}
-                        prefix={<CopyOutlined/>}/>
+                        prefix={<CopyOutlined/>}
+                    />
                 </Col>
+            </Row>
+            <Divider/>
+            <Title level={3}>Последний добавленный документ: </Title>
+            <Row>
+                <div style={{padding: 30, backgroundColor: 'white', border: '1px solid #ebebeb', borderRadius: 10}}>
+                    <PageHeader
+                        title={props.lastAddedMainDoc.name_ru}
+                        className="site-page-header"
+                        subTitle={props.lastAddedMainDoc.number}
+                        tags={
+                            <div>
+                                <Tag color={'purple'}>{getDepartmentText(props.lastAddedMainDoc.department_id)}</Tag>
+                                <Tag
+                                    color={getStatusColor(props.lastAddedMainDoc.status_id)}>{getStatusText(props.lastAddedMainDoc.status_id)}</Tag>
+                            </div>
+                        }
+                        avatar={{src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4'}}
+                    >
+                        <Content
+                            extraContent={
+                                <img
+                                    src={word_svg}
+                                    alt="content"
+                                    width={80}
+                                    height={80}
+                                />
+                            }
+                        >
+                            <Descriptions column={1} style={{textAlign: "left", marginTop: 20}}>
+                                <Descriptions.Item label={'Заголовок документа'}>
+                                    {props.lastAddedMainDoc.header_ru}
+                                </Descriptions.Item>
+                                <Descriptions.Item label={'Дата публикации'}>
+                                    {props.lastAddedMainDoc.pub_date}
+                                </Descriptions.Item>
+                            </Descriptions>
+
+                            <div style={{textAlign: 'left'}}>
+                                <Button
+                                    style={{marginTop: 20, marginRight: 10}}
+                                    type="danger"
+                                    shape="round"
+                                >
+                                    {t('more')}
+                                </Button>
+                                <Button
+                                    style={{marginTop: 20, marginLeft: 10}}
+                                    type="primary"
+                                    shape="round"
+                                    icon={<DownloadOutlined/>}
+                                >
+                                    {t('download')}
+                                </Button>
+                            </div>
+                        </Content>
+                    </PageHeader>
+                </div>
             </Row>
         </div>
     )
--- arm-tipo/src/components/WorkSpace/Home/HomeContainer.jsx
+++ arm-tipo/src/components/WorkSpace/Home/HomeContainer.jsx
@@ -2,20 +2,28 @@ import React from 'react'
 import Home from "./Home";
 import {connect} from "react-redux";
 import {getUsers} from "../../../redux/Reducers/UserReducer";
+import {getMainDocs} from "../../../redux/Reducers/MainDocReducer";
+import {Spin} from "antd";
 
 class HomeContainer extends React.Component {
     componentDidMount() {
-        !this.props.usersCount && this.props.getUsers()
+        !this.props.usersCount && this.props.getUsers();
+        !this.props.mainDocsDir.mainDocsCount && this.props.getMainDocs();
     }
 
     render() {
-        return (
-            <Home
-                usersCount={this.props.usersCount}
-                mainDocsCount={this.props.mainDocsCount}
-                isFetching={this.props.isFetching}
-            />
-        )
+        if (!this.props.mainDocsDir.mainDocs.length) {
+            return <Spin/>
+        } else {
+            return (
+                <Home
+                    usersCount={this.props.usersCount}
+                    lastAddedMainDoc={this.props.mainDocsDir.mainDocs[this.props.mainDocsDir.mainDocs.length - 1]}
+                    mainDocsCount={this.props.mainDocsDir.mainDocsCount}
+                    isFetching={this.props.isFetching}
+                />
+            )
+        }
     }
 }
 
@@ -23,12 +31,13 @@ let mapStateToProps = (state) => {
     return {
         usersCount: state.usersDir.usersCount,
         isFetching: state.usersDir.isFetching,
-        mainDocsCount: state.mainDocsDir.mainDocsCount
+        mainDocsDir: state.mainDocsDir
     }
 };
 
 export default connect(mapStateToProps,
     {
-        getUsers
+        getUsers,
+        getMainDocs
     }
 )(HomeContainer);
