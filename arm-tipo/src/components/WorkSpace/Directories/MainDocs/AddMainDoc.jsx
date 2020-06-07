aca7f2b121854c9749c58b46ab318e6c7d3ed784
valentin.kulik <valentin.kulik.00@mail.ru>
2020-06-07 15:09:20 +0600

    [#15] Date picker localization

--- arm-tipo/src/components/WorkSpace/Directories/MainDocs/AddMainDoc.jsx
+++ arm-tipo/src/components/WorkSpace/Directories/MainDocs/AddMainDoc.jsx
@@ -247,7 +247,7 @@ const AddMainDoc = (props) => {
                     <RangePicker
                         format={'YYYY-MM-DD'}
                         onChange={changeBeginAndFinishDate}
-                        placeholder={["Вступил в силу", "Утратил силу"]}
+                        placeholder={[t("published"), t("finished")]}
                     />
                 </Form.Item>
 
